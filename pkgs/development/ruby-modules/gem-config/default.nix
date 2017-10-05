1ee312c6d8bd62dd491727092875969a6a6803d4
Aneesh Agrawal <aneeshusa@gmail.com>
2017-10-05 04:10:57 -0400

    ruby treewide: don't merge source into top-level
    Keep the `source` attrset distinct to prevent its entries from merging
    with the top level attrs.
    Since each type of source has a different set of entries for `source`,
    this is the easiest way to keep them together.
    This will pave the way for a new `url` type of source.
    
    This is a mass-rebuild of many ruby packages,
    notably those using `git` type sources.

--- pkgs/development/ruby-modules/gem-config/default.nix
+++ pkgs/development/ruby-modules/gem-config/default.nix
@@ -32,7 +32,7 @@ let
     name = "rake";
     gemName = "rake";
     remotes = ["https://rubygems.org"];
-    sha256 = "01j8fc9bqjnrsxbppncai05h43315vmz9fwg28qdsgcjw9ck1d7n";
+    source.sha256 = "01j8fc9bqjnrsxbppncai05h43315vmz9fwg28qdsgcjw9ck1d7n";
     type = "gem";
     version = "12.0.0";
   };
