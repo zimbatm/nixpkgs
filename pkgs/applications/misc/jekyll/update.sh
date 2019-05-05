0078667f8e9246bb3a35c439b8ff5a194b208164
zimbatm <zimbatm@zimbatm.com>
2019-05-05 15:58:58 +0200

    jekyll: add a github-pages plugins set

--- pkgs/applications/misc/jekyll/update.sh
+++ pkgs/applications/misc/jekyll/update.sh
@@ -6,7 +6,7 @@ set -o nounset
 
 readonly BASEDIR="$(dirname $(readlink -f $0))"
 
-for directory in "basic" "full"; do
+for directory in "basic" "full" "github-pages"; do
   pushd "$BASEDIR/$directory"
   rm -f Gemfile.lock gemset.nix
   bundix --magic
